c89ed44107f1c9941dd8deee0ad77dc7d223c76f
Masato Ikeda <masato.ikeda@gmail.com>
2019-11-05 00:12:50 +0900

    Introduce Cursor object
    https://cstack.github.io/db_tutorial/parts/part6.html

--- main.c
+++ main.c
@@ -160,13 +160,44 @@ void db_close(Table *table) {
   free(table);
 }
 
-// Returns a pointer where the row specified with args should be serialized to.
-void *row_slot(Table *table, uint32_t row_num) {
+typedef struct {
+  Table *table;
+  uint32_t row_num;
+  bool end_of_table; // Indicates whether the table has a record here or not
+} Cursor;
+
+Cursor *table_start(Table *table) {
+  Cursor *cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = 0;
+  cursor->end_of_table = (table->num_rows == 0);
+  return cursor;
+}
+
+// Returns a cursor which represents where a new record should be inserted to.
+Cursor *table_end(Table *table) {
+  Cursor *cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->row_num = table->num_rows;
+  cursor->end_of_table = true;
+  return cursor;
+}
+
+// Returns a pointer which the cursor points.
+void *cursor_value(Cursor *cursor) {
+  uint32_t row_num = cursor->row_num;
   uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void *page = get_page(table->pager, page_num);
+  void *page = get_page(cursor->table->pager, page_num);
   uint32_t row_offset = row_num % ROWS_PER_PAGE;
   uint32_t byte_offset = row_offset * ROW_SIZE;
   return page + byte_offset;
+}
+
+void cursor_advance(Cursor *cursor) {
+  cursor->row_num += 1;
+  if (cursor->row_num >= cursor->table->num_rows) {
+    cursor->end_of_table = true;
+  }
 }
 
 typedef struct {
@@ -309,19 +340,23 @@ ExecuteResult execute_insert(Statement *statement, Table *table) {
   }
 
   Row *row_to_insert = &(statement->row_to_insert);
-
-  serialize_row(row_to_insert, row_slot(table, table->num_rows));
+  Cursor *cursor = table_end(table);
+  serialize_row(row_to_insert, cursor_value(cursor));
   table->num_rows += 1;
+  free(cursor);
 
   return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement *statement, Table *table) {
   Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++) {
-    deserialize_row(row_slot(table, i), &row);
+  Cursor *cursor = table_start(table);
+  while (!(cursor->end_of_table)) {
+    deserialize_row(cursor_value(cursor), &row);
     print_row(&row);
+    cursor_advance(cursor);
   }
+  free(cursor);
   return EXECUTE_SUCCESS;
 }
 
