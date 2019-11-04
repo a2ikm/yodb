41128f99e845cc7ce234b73b696d1ab26676ec19
Masato Ikeda <masato.ikeda@gmail.com>
2019-11-04 12:21:59 +0900

    Implemented negative id error

--- main.c
+++ main.c
@@ -59,7 +59,8 @@ typedef enum {
   PREPARE_SUCCESS,
   PREPARE_UNRECOGNIZED_STATEMENT,
   PREPARE_SYNTAX_ERROR,
-  PREPARE_STRING_TOO_LONG
+  PREPARE_STRING_TOO_LONG,
+  PREPARE_NEGATIVE_ID
 } PrepareResult;
 
 typedef enum {
@@ -98,6 +99,9 @@ PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement) {
   }
 
   int id = atoi(id_string);
+  if (id < 0) {
+    return PREPARE_NEGATIVE_ID;
+  }
 
   if (strlen(username) > COLUMN_USERNAME_SIZE) {
     return PREPARE_STRING_TOO_LONG;
@@ -250,6 +254,9 @@ int main(int arc, char *argv[]) {
         continue;
       case PREPARE_STRING_TOO_LONG:
         printf("String is too long.\n");
+        continue;
+      case PREPARE_NEGATIVE_ID:
+        printf("ID must be positive.\n");
         continue;
     }
 
