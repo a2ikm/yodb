1d4d7c081968e9db01475aba4b127bc3a85ea154
Masato Ikeda <masato.ikeda@gmail.com>
2019-11-06 01:14:12 +0900

    Added .btree command

--- main.c
+++ main.c
@@ -326,6 +326,15 @@ void print_constants() {
   printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
 }
 
+void print_leaf_node(void *node) {
+  uint32_t num_cells = *leaf_node_num_cells_ptr(node);
+  printf("leaf (size %d)\n", num_cells);
+  for (uint32_t i = 0; i < num_cells; i++) {
+    uint32_t key = *leaf_node_key_ptr(node, i);
+    printf("  - %d : %d\n", i, key);
+  }
+}
+
 typedef enum {
   META_COMMAND_SUCCESS,
   META_COMMAND_UNRECOGNIZED_COMMAND
@@ -335,6 +344,9 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table) {
   if (strcmp(input_buffer->buffer, ".exit") == 0) {
     db_close(table);
     exit(EXIT_SUCCESS);
+  } else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+    print_leaf_node(get_page(table->pager, 0));
+    return META_COMMAND_SUCCESS;
   } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
     print_constants();
     return META_COMMAND_SUCCESS;
